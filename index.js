7ee7f524bca690d4721520397292b701a2c495ec
bjw123 <brycewilkinson43@gmail.com>
2020-08-18 14:03:23 +0900

    fixed post request
    added nodemail for mailing service

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 const uri = "mongodb+srv://admin:admin@cluster0.5cdt0.mongodb.net/test?retryWrites=true&w=majority"
 const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
+const nodemailer = require('nodemailer');
 
 let app = new express();
 
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
 //test ignore
 app.get("/",(req, res) => {
--- public/Login.js
+++ public/Login.js
@@ -1,32 +1,35 @@
-console.log("login script loaded");
+const newUser=()=>{
+    let name = $("#user-name").val();
+    let email = $("#user-email").val();
+    let age = $("#user-age").val();
+    let gender = $("#user-gender").val();
+    console.log(name)
+    console.log(email)
 
-/*
-function show_value(x)
-{
-    document.getElementById("slider_value").innerHTML=x;
-}*/
+    let data = {name:name, email:email, age:age, gender:gender}
+    console.log("data:    " +data)
+    $.ajax({
+        url: '/createUser',
+        contentType: 'application/json',
+        data: JSON.stringify(data), // access in body
+        type: 'POST',
+        success: function (result) {
+            console.log(data)
+            console.log(result)
+            window.location.replace("Profile.html");
+            //.html?email="x"
+        }
+    })
+}
 
 $(document).ready(function () {
 
     $('select').formSelect();
 
     $("#submit-button").click(function () {
-        let name = $("#user-name").val();
-        let email = $("#user-email").val();
-        let age = $("#user-age").val();
-        let gender = $("#user-gender").val();
+        newUser();
 
-        let data = {name:name, email:email, age:age, gender:gender}
-        $.ajax({
-            url: '/createUser',
-            contentType: 'application/json',
-            data: JSON.stringify(data), // access in body
-            type: 'POST',
-            success: function(result) {
-                console.log(result)
-                window.location.replace("Profile.html");
-            }
-        });
+
     })
 
 
